d62c99c80aefc5a7f6444f5abbdaba112bcaa074
Matt Lee <matthew.lee@abletech.co.nz>
2017-11-17 16:55:53 +1300

    CC#9 Refactor Batch model test

--- test/models/batch_test.rb
+++ test/models/batch_test.rb
@@ -1,86 +1,86 @@
 require 'test_helper'
+#
+# class BatchTest < ActiveSupport::TestCase
+#   describe Batch do
+#     let(:roast) { roasts(:roast1) }
+#
+#     describe 'start_date' do
+#       it 'is allowed to be today' do
+#         batch = Batch.new({:start_date => Date.today, :amount_purchased => 22, :cost => 4.5, :roast => roast})
+#         assert batch.valid?
+#       end
+#
+#     end
+#
+#   end
+# end
 
 class BatchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
   setup do
     @roast = roasts(:roast1)
+    @valid_params = {:start_date => Date.today, :amount_purchased => 22, :cost => 4.5, :roast => @roast}
+
+    @batch = Batch.new(@valid_params)
   end
 
-  test 'required batch fields must not be empty' do
-    batch = Batch.new
-    assert batch.invalid?
-    batch.start_date = 5.days.ago
-    assert batch.invalid?
-    batch.amount_purchased = 22
-    assert batch.invalid?
-    batch.cost = 2.2
-    assert batch.invalid?
+  test 'is valid' do
+    assert @batch.valid?
   end
 
-  test 'start_date must be earlier (or) today' do
-    batch = Batch.new
-    batch.amount_purchased = 200
-    batch.cost = 22
-    batch.roast = @roast
-    batch.start_date = Date.today
-    assert batch.valid?
-    batch.start_date = 5.days.ago
-    assert batch.valid?
+  [:start_date, :amount_purchased, :cost, :roast_id].each do |param|
+    test "#{param} is required" do
+      @batch[param] = nil
+
+      assert @batch.invalid?
+    end
+  end
+
+  test 'start_date is allowed to be today' do
+    @batch.start_date = Date.today
+
+    assert @batch.valid?
+  end
+
+  test 'start_date must be earlier than today' do
+    @batch.start_date = 5.days.ago
+
+    assert @batch.valid?
   end
 
   test 'start_date cannot be after today' do
-    batch = Batch.new
-    batch.amount_purchased = 200
-    batch.cost = 22
-    batch.roast = @roast
-    batch.start_date = 2.days.from_now
-    assert batch.invalid?
+    @batch.start_date = 5.days.from_now
+
+    assert @batch.invalid?
   end
 
   test 'amount_purchased cannot be negative' do
-    batch = Batch.new
-    batch.amount_purchased = -4
-    batch.cost = 22
-    batch.roast = @roast
-    batch.start_date = 2.days.ago
-    assert batch.invalid?
+    @batch.amount_purchased = -45
+
+    assert @batch.invalid?
   end
 
   test 'cost cannot be negative' do
-    batch = Batch.new
-    batch.amount_purchased = 22
-    batch.cost = -11
-    batch.roast = @roast
-    batch.start_date = 2.days.ago
-    assert batch.invalid?
+    @batch.cost = -55
+
+    assert @batch.invalid?
   end
 
   test 'cost can be $0' do
-    batch = Batch.new
-    batch.amount_purchased = 22
-    batch.cost = 0
-    batch.roast = @roast
-    batch.start_date = 2.days.ago
-    assert batch.valid?
+    @batch.cost = 0
+
+    assert @batch.valid?
   end
 
   test 'cannot add two batches with the same start_date' do
-    batch = Batch.new
-    batch.amount_purchased = 200
-    batch.cost = 22
-    batch.roast = @roast
-    batch.start_date = Date.today
-    assert batch.save
+    @batch.start_date = Date.today
+    @batch.save
 
-    batch2 = Batch.new
-    batch.amount_purchased = 200
-    batch.cost = 22
-    batch.roast = @roast
-    batch.start_date = Date.today
-    assert !batch2.save
+    assert @batch.valid?
+
+    batch2 = Batch.new(@valid_params)
+    batch2.start_date = Date.today
+
+    assert batch2.invalid?
   end
-
 end
