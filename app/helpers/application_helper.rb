5c26a23e039bdcea4354f5eda1595680dcc4d448
Matt Lee <matthew.lee@abletech.co.nz>
2017-11-22 09:26:17 +1300

    CC#8 Fix up multiple data queries and tidied up views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,2 @@
 module ApplicationHelper
-  def format_roast_options(roasts)
-    roasts.collect{|r| ["#{r.company} #{r.name}", r.id]}
-  end
-
-  def calculate_score(stats)
-    stats[:good] - stats[:bad]
-  end
-
-  def sort_report(report)
-    report.sort_by do |roast, stats|
-      calculate_score(stats[:good], stats[:bad])
-    end
-  end
-
-  def good_score(roast)
-    roast[:roast][:good]
-  end
 end
