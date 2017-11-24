63dd68fd9441e5ee0d9114746b45e746d593f9ae
Matt Lee <matthew.lee@abletech.co.nz>
2017-11-24 14:21:10 +1300

    CC#12 Refactor create vote report

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,14 @@
 class WelcomeController < ApplicationController
   def index
     batches = Batch.preload(:roast)
-    report = CreateVotingReport.new(batches, Vote.all).perform
+    stats_for_roast_report = CreateVotingReport.new(batches, Vote.all).perform
 
     @active_batch = batches.active
 
     if @active_batch.present?
       @active_roast = @active_batch.roast
-      @active_roast_scores = report[@active_roast]
-      @sorted_report = report.sort_by { |_, stats| stats[:score] }.reverse[0..9]
+      @active_roast_scores = stats_for_roast_report[@active_roast]
+      @sorted_report = stats_for_roast_report.sort_by { |_, stats| stats[:score] }.reverse[0..9]
     end
   end
 end
